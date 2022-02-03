1bcb7521742906680031edae1fe2299720de9663
SSJenny90 <samuel.scott.jennings@gmail.com>
2022-02-03 20:06:00 +1030

    refactored

--- login/abstract.py
+++ login/abstract.py
@@ -1,34 +1,39 @@
-from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from .managers import UserManager
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-class UserManager(BaseUserManager):
-    """Define a model manager for User model with no username field."""
+class NoUsernameAbstractUser(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
 
-    use_in_migrations = True
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name','last_name']
 
-    def _create_user(self, email, password, **extra_fields):
-        """Create and save a User with the given email and password."""
-        if not email:
-            raise ValueError('The given email must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+    objects = UserManager()
 
-    def create_user(self, email, password=None, **extra_fields):
-        """Create and save a regular User with the given email and password."""
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
+    class Meta:
+        abstract=True
 
-    def create_superuser(self, email, password, **extra_fields):
-        """Create and save a SuperUser with the given email and password."""
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+    def save(self,*args, **kwargs):
+        super().save(*args, **kwargs)
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
+    def __str__(self):
+        return str(self.username)
 
-        return self._create_user(email, password, **extra_fields)
+    def get_full_name(self):
+        """This method is used by the comments framework as a display name"""
+        return self.first_name
+
+    def full_name(self):
+        return '{} {}'.format(self.first_name,self.last_name)
+
+    def display_name(self):
+        return '{}.{}'.format(self.first_name[0],self.last_name)
+
+    def initials(self):
+        return '{}{}'.format(self.first_name[0],self.last_name[0])
+
+    @property
+    def username(self):
+        return self.display_name()
