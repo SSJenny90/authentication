1f83dc6e988a6960370dbd61816a2bf6f27e991e
SSJenny90 <samuel.scott.jennings@gmail.com>
2022-02-07 18:00:59 +1030

    consistent links to auth modals

--- authentication/templatetags/authentication.py
+++ authentication/templatetags/authentication.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from allauth.utils import get_form_class
 from crispy_forms.utils import render_crispy_form
 from django.template.context_processors import csrf
+from django.template.loader import render_to_string
 
 register = template.Library()
 
@@ -13,3 +14,9 @@ def authentication_form(context,key):
     if not form:
         raise KeyError(f'Could not find form using the lookup key: {key}. Possible values are {list(settings.ACCOUNT_FORMS.keys())}')
     return render_crispy_form(form, context=csrf(context['request']))
+
+
+@register.simple_tag
+def auth_link(type, link_text):
+    template = f'authentication/links/{type}.html'
+    return render_to_string(template, {'link_text': link_text})
