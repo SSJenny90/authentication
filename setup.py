a583a8e0ed69b573866ed257e6e4a16f2aca876e
SSJenny90 <samuel.scott.jennings@gmail.com>
2022-02-12 20:23:06 +1030

    changes

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+from core import __version__
+
+REPO_URL = "https://github.com/SSJenny90/authentication"
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='authentication',
+    packages=find_packages(),
+    include_package_data=True,
+    version=__version__,
+    author='Sam Jennings',
+    author_email='samuel.jennings@pm.me',
+    license='MIT',
+    description='Authentication system for GeoLuminate products',
+    url=REPO_URL,
+    install_requires=[
+        "Django>=3,<4",    
+        "django-all-auth",   
+        ],
+    keywords='scientific django',
+    classifiers=[
+        'Development Status :: 1 - Development',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+        'Natural Language :: English',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
