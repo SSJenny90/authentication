5f815369a4eaccf6a50e58b8ced926fc878f3d8e
SSJenny90 <samuel.scott.jennings@gmail.com>
2022-03-21 17:39:45 +1030

    styling

--- authentication/templatetags/authentication.py
+++ authentication/templatetags/authentication.py
@@ -33,3 +33,7 @@ def auth_link(modal_id, link_text, classes=''):
 @register.simple_tag(takes_context=True)
 def can_add_email(context):
     return EmailAddress.objects.can_add_email(context['request'].user)
+
+@register.filter(name='has_group')
+def has_group(user, group_name):
+    return user.groups.filter(name=group_name).exists()
